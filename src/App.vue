f990d237cfcc05813dbb72a4884eaad2f4fead8e
Milos Stojsavljevic <m.stojsavljevic@findologic.com>
2021-08-01 13:05:17 +0200

    Add key

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <body>
   <div>
     <Navbar />
-    <router-view v-slot="{ Component }">
+    <router-view :key="$route.fullPath" v-slot="{ Component }">
       <transition
       mode="out-in"
       enter-active-class="animate__animated animate__fadeIn"
